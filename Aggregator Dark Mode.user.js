b566a00781689d3749f5e838fe30baf5d36f8bae
Besnik Nuro <besniknuro74@gmail.com>
2021-08-05 23:10:35 +0200

    Update Aggregator Dark Mode.user.js

--- Aggregator Dark Mode.user.js
+++ Aggregator Dark Mode.user.js
@@ -2,11 +2,11 @@
 // @name         Aggregator Dark Mode
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @icon         https://news.ycombinator.com/y18.gif
+// @icon				 https://news.ycombinator.com/y18.gif
 // @description  Simple Dark Mode Stylesheet for HN.
 // @match        https://news.ycombinator.com/* 
-// @match        https://lobste.rs/*
-// @match        https://lemmy.ml/*
+// @match				 https://lobste.rs/*
+// @match				 https://lemmy.ml/*
 // @grant        All
 // ==/UserScript==
 
@@ -15,13 +15,14 @@
     var style = document.createElement("style");
     style.type = "text/css";
     style.innerHTML = (function EOF(){ /*!<<<EOF
-body  { font-family:Verdana, Geneva, sans-serif; font-size:10pt; color:#999999; background-color: #131b28; }
-#hnmain { background-color: #313140; }
+body  { font-family:Verdana, Geneva, sans-serif; font-size:10pt; color:#999999; background-color: #000001;
+background-image: url("https://raw.githubusercontent.com/Besnn/userscripts/master/assets/stars.gif") }
+#hnmain { background-color: #181c5c; }
 .admin td   { font-family:Verdana, Geneva, sans-serif; font-size:8.5pt; color:#000000; }
-textarea { font-family:monospace; font-size:10pt; background: #181f35; color: #EEE; }
+textarea { font-family:monospace; font-size:10pt; background: #9090A0; color: #111111; }
 a:link    { color:#CCCCCC; text-decoration:underline; }
 a:visited { color:#9999ca; text-decoration:underline; }
-.pagetop { font-family:Verdana, Geneva, sans-serif; font-size: 10pt; color:#777777; }
+.pagetop { font-family:Verdana, Geneva, sans-serif; font-size: 10pt; color:#cccccc; }
 .c00, .c00 a:link { color:#DDDDDD; }
 .pagetop a:visited { color:#000000;}
 .topsel a:link, .topsel a:visited { color:#ffffff; }
